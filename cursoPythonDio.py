0d4748202b2c23de2edb1c930c156743fe431844
EmilyBirschner <emilyb.oliveira@hotmail.com>
2024-04-05 15:44:31 -0300

    Variáveis

--- cursoPythonDio.py
+++ cursoPythonDio.py
@@ -1,2 +1,6 @@
-age, name = {35, 'Emily'}
+name = 'Maria'
+age = 28
+print(f'Meu nome é {name} e eu tenho {age} anos de idade.')
+
+name, age = {'João', 30}
 print(f'Meu nome é {name} e eu tenho {age} anos de idade.')
