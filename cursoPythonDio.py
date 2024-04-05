513cf221927a1088d07d0923035ec36569324d11
EmilyBirschner <emilyb.oliveira@hotmail.com>
2024-04-05 17:03:00 -0300

    Entradas e saídas

--- cursoPythonDio.py
+++ cursoPythonDio.py
@@ -1,22 +1,3 @@
-preco = 10
-print(preco)
-
-preco = int(preco)
-print(preco)
-
-preco = float(preco)
-print(preco)
-
-preco = 10.50
-idade = 28
-print(str(preco))
-print(str(idade))
-
-texto = f'idade {idade} preço {preco}'
-print(texto)
-
-valor = 10
-valor_str = str(valor)
-print(type(valor))
-print(type(valor_str))
-
+nome = input('Informe seu nome: ')
+idade = input('Informe sua idade: ')
+print(nome, idade, sep='#')
