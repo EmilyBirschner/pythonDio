e5f1a3f590a7585d710624e77e3d0516c48d892a
EmilyBirschner <emilyb.oliveira@hotmail.com>
2024-04-24 14:11:02 -0300

    Range

--- estruturas_repeticao.py
+++ estruturas_repeticao.py
@@ -1,10 +1,2 @@
-texto = input("informe um texto: ")
-VOGAIS = "AEIOU"
-
-for letra in texto:
-    if letra.upper() in VOGAIS:
-        print(letra, end= "")
-        
-else: #não é muito comum
-    print() #adiciona uma quebra de linha
-    print("Escuta no final do laço!")
+for numero in range (0, 51, 5):
+    print(numero, end=" ")
