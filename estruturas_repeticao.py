a3d670676479c75e522a81b243392816cd4b81c2
EmilyBirschner <emilyb.oliveira@hotmail.com>
2024-04-24 14:16:54 -0300

    while

--- estruturas_repeticao.py
+++ estruturas_repeticao.py
@@ -1,2 +1,9 @@
-for numero in range (0, 51, 5):
-    print(numero, end=" ")
+opcao = -1
+
+while opcao != 0:
+    opcao = int(input("[1] Sacar \n[2] Extrato \n[0] Sair "))
+    
+    if opcao == 1:
+        print("Sacando...")
+    elif opcao == 2:
+        print("Exibindo o extrato...")
