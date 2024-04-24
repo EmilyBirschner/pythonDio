a5c5bc966668d6e43b1271329dd19b3888674644
EmilyBirschner <emilyb.oliveira@hotmail.com>
2024-04-24 15:30:38 -0300

    f-string

---
+++ interpolacao_variaveis.py
@@ -0,0 +1,9 @@
+nome = "Emily"
+idade = 28
+profissao = "Programadora"
+linguagem = "Python"
+
+print(f'Olá, me chamo {nome}. E eu tenho {idade} anos de idade, trabalho como {profissao} e estou matriculada no curso de {linguagem}')
+
+PI = 3.14159
+print(f'Valor de PI: {PI:.2f}')
