bd2de63d884d103802ddf7893bb6daf94fa31241
EmilyBirschner <emilyb.oliveira@hotmail.com>
2024-06-18 12:46:01 -0300

    conversão de datas com máscara

--- 04 - Data e hora/3_strftime_strptime.py
+++ 04 - Data e hora/3_strftime_strptime.py
@@ -5,8 +5,8 @@ data_hora_str = "2023-10-20 10:20"
 mascara_ptbr = "%d/%m/%Y %a"
 mascara_en = "%Y-%m-%d %H:%M"
 
-print(data_hora_atual.strftime(mascara_ptbr))
+print(f"data com máscara pt-br{data_hora_atual.strftime(mascara_ptbr)}")
 
 data_convertida = datetime.strptime(data_hora_str, mascara_en)
-print(data_convertida)
-print(type(data_convertida))
+print(f'data convertida: {data_convertida}')
+print(f'Tipo da data convertida: {type(data_convertida)}')
