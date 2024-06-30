bdb6cdab4e44dbc12b9496891994422a808254b1
EmilyBirschner <emilyb.oliveira@hotmail.com>
2024-06-30 12:51:27 -0300

    escrevendo em um arquivo

--- 05 - Manipulação de arquivos/operacao_escrita.py
+++ 05 - Manipulação de arquivos/operacao_escrita.py
@@ -1,4 +1,4 @@
 arquivo = open("C:\\Users\\Emily\\Documents\\Projetos\\pythonDio\\05 - Manipulação de arquivos\\teste.txt","w")
 arquivo.write("novo texto para teste ")
-arquivo.writelines([" escrevendo ", "um novo ", "texto"])
+arquivo.writelines(["\n","escrevendo ", "\num novo ", "\ntexto"])
 arquivo.close()
